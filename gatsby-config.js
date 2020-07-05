053956f7e1ec973d5bb9a1de174388dff72d68af
Elisheba <leighw33@gmail.com>
2020-07-05 10:58:37 -0400

    Update photo and clean up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
+  siteMetadata: {
+    description: "Personal page of Elisheba Anderson",
+    locale: "en",
+    title: "Elisheba Anderson",
+    showThemeLogo: true
+  },
   plugins: [
     {
       resolve: "@wkocjan/gatsby-theme-intro",
       options: {
+        basePath: "/",
+        contentPath: "content/",
         theme: "classic"
       }
     }
